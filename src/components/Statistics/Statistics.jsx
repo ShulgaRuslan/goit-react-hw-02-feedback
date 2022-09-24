55078a66ea03259217ac65d2b4024adae0cce2a3
ShulgaRuslan <shulgar1988@gmail.com>
2022-09-25 02:48:39 +0300

    finish

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,29 +1,32 @@
-export const Statistics = ({ good, neutral, bad }) => {
-    return (
-        <ul className="list">
-            <li className="item">
-                <span className="">Good:</span>
-            </li>
-            <li className="item">
-                <span className="">Neutral:</span>
-            </li>
-            <li className="item">
-                <span className="">Bad:</span>
-            </li>
+import React from 'react';
+import PropTypes from 'prop-types';
+import css from "./Statistics.module.css";
 
-        </ul>
-    )
+export default function Statistics({
+  good,
+  neutral,
+  bad,
+  total,
+  positivePercentage,
+}) {
+  return (
+    <div>
+      <ul className={css.feedback__list}>
+        <li className={css.feedback_list__item}>Good: <span className={css.statistic_item__data}>{good}</span></li>
+        <li className={css.feedback_list__item}>Neutral: <span className={css.statistic_item__data}>{neutral}</span></li>
+        <li className={css.feedback_list__item}>Bad: <span className={css.statistic_item__data}>{bad}</span></li>
+        <li className={css.feedback_list__item}>Total: <span className={css.statistic_item__data}>{total}</span></li>
+        <li className={css.feedback_list__item}>Positive feedback: <span className={css.statistic_item__data}>{positivePercentage}%</span></li>
+      </ul>
+    </div>
+  );
 }
 
+Statistics.propTypes = {
+    good: PropTypes.number,
+    neutral: PropTypes.number,
+    bad: PropTypes.number,
+    total: PropTypes.number,
+    positivePercentage: PropTypes.number,
 
-
-
-
-
-
-
-state = {
-  good: 0,
-  neutral: 0,
-  bad: 0
 }
